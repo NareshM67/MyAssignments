0b067f8470d6d56763fcd31a1d35eed44852a7ef
Naresh M <nareshchary.m@gmail.com>
2024-06-18 17:03:19 +0530

    Add files via upload
    Day1

---
+++ day1/FindOddNumbers.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class FindOddNumbers 
+{
+
+	public static void main(String[] args) 
+	{		
+		int maxRange=10;  
+		System.out.print("List of odd numbers : ");  
+		for (int i=1; i<=maxRange; i++)   
+		{  		
+			if (i%2==1)   
+			{  
+				System.out.print(i + " ");  
+			}	
+		}	
+	}  
+		
+
+}
+
